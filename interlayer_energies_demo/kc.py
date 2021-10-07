9b0a0f9d681df4b127fe651cc250881b6048c50e
Mick Krongchon <krongch2@illinois.edu>
2021-10-07 16:26:38 -0500

    set mol-id to distinguish carbons from different layers

--- interlayer_energies_demo/kc.py
+++ interlayer_energies_demo/kc.py
@@ -11,12 +11,12 @@ import load
 def eval_energy(df, z0, C0, C2, C4, C, delta, lamda, A):
     """
     """
-
-    os.system('rm -r tmp_dir/')
+    os.system('rm -r tmp/')
     energy = []
     for it, row in df.iterrows():
+        print(row['d'], row['disregistry'])
         atoms = generate_geometry.create_graphene_geom(row['d'], row['disregistry'])
-        print('\n'.join(dir(atoms)))
+        atoms.set_array('mol-id', np.array([0, 0, 1, 1]))
         parameters = {
             'pair_style': 'hybrid/overlay rebo kolmogorov/crespi/full 16.0 1',
             'pair_coeff': [
@@ -26,20 +26,15 @@ def eval_energy(df, z0, C0, C2, C4, C, delta, lamda, A):
                 ],
             'atom_style': 'full',
             'specorder': ['C', 'C'],
-            'mol-id': [1, 1, 2, 2]
             }
         files = ['CH.rebo', 'CH_taper.KC']
-        lammps = LAMMPS(parameters=parameters, files=files, keep_tmp_files=True, tmp_dir='tmp_dir/')
-        atoms.calc = lammps
-        print('*'*50)
-        print(atoms.get_potential_energy()/len(atoms))
-        print('after')
-        energy.append(atoms.get_potential_energy()/len(atoms))
-        break
+        atoms.calc = LAMMPS(parameters=parameters, files=files, keep_tmp_files=True, tmp_dir='tmp/')
+        e = atoms.get_potential_energy()/len(atoms)
+        print(e)
+        energy.append(e)
 
     lj_en =  np.asarray(energy) - np.min(energy) + np.min(df['energy'])
     return lj_en
-
 
 df = load.load_data()
 eval_energy(df, 3.416084, 20.021583, 10.9055107, 4.2756354, 1.0010836E-2, 0.8447122, 2.9360584, 14.3132588)
