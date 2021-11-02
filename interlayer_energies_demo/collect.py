518ed48bfe0237fe3b2ad8d0b3061acbe7733577
krongch2 <krongch2@illinois.edu>
2021-11-02 11:12:45 -0500

    .

--- interlayer_energies_demo/collect.py
+++ interlayer_energies_demo/collect.py
@@ -18,7 +18,7 @@ def create_fitted_df():
     df_fit = load.load_data()
     popt = (3.372485976907569, 18.172084448730008, 3.573924286622721, 12.909862639795223, 5.68180602644627, 0.6484292762516102, 3.1279865162981135, 15.244928020134566) # results for gtol=1e-06
     df_fit['en'] = eval_energy(df_fit, *popt)
-    df_fit['label'] = 'fit'
+    df_fit['label'] = 'qmc'
     df = pd.concat([df_orig, df_fit], ignore_index=True)
     df['stacking'] = df['disregistry'].map({0.0: 'AB', 0.16667: 'SP', 0.5: 'Mid', 0.66667: 'AA'})
     print(df)
@@ -26,11 +26,17 @@ def create_fitted_df():
 
 def plot():
     df = pd.read_csv('kc.csv')
-    g = sns.FacetGrid(data=df, col='stacking', hue='label')
+    df['label'] = df['label'].map({'orig': 'DFT fitted', 'fit': 'QMC fitted'})
+    print(df)
+    g = sns.FacetGrid(data=df, col='stacking', hue='label', sharey='row')
     g.map(plt.plot, 'd', 'en')
     g.map(plt.errorbar, 'd', 'energy', 'energy_err', marker='o', alpha=0.7, ms=3, linestyle="")
-    g.add_legend()
-    plt.savefig('kc.png')
+    for i, (stacking, ax) in enumerate(g.axes_dict.items()):
+        if i == 0:
+            ax.set(ylabel='$E$ (eV/atom)')
+    g.set(xlabel='$d$ (ang)')
+    g.add_legend(bbox_to_anchor=(0.9, 0.7), fontsize=9)
+    plt.savefig('kc_refit.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
     plot()
