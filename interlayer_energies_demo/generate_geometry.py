d504777afea0de14d4a0ae7dedfba0af22400e73
Lucas Wagner <lucas.wagner@gmail.com>
2021-11-05 09:37:49 -0500

    Revert "set mol-id to distinguish carbons from different layers"
    This reverts commit 9b0a0f9d681df4b127fe651cc250881b6048c50e.

--- interlayer_energies_demo/generate_geometry.py
+++ interlayer_energies_demo/generate_geometry.py
@@ -39,87 +39,13 @@ def create_graphene_geom(d,disregistry, a=2.462,c=15, zshift='CM'):
     All units should be in angstroms"""
     symbols = ['C']*4
     # symbols = ['C1', 'C1', 'C2', 'C2']
-    atoms = ase.Atoms(
+    return ase.Atoms(
         symbols=symbols,
         positions=get_basis(a, d, c, disregistry, zshift=zshift),
         cell=get_lattice_vectors(a, c),
         pbc=[1, 1, 1],
-        # masses=[12, 12, 12, 12],
-        tags=[0, 0, 1, 1],
+        # masses=[12, 12, 12, 12]
         )
-    return
-
-def get_basis_bottom(a, d, c, disregistry, zshift='CM'):
-
-    '''
-    `disregistry` is defined such that the distance to disregister from AB to AB again is 1.0,
-    which corresponds to 3*bond_length = 3/sqrt(3)*lattice_constant = sqrt(3)*lattice_constant
-    so we convert the given `disregistry` to angstrom
-    '''
-    disregistry_ang = 3**0.5*a*disregistry
-    orig_basis = np.array([
-        [0, 0, 0],
-        [0, a/3**0.5, 0],
-        ])
-
-    # for open boundary condition in the z-direction
-    # move the first layer to the middle of the cell
-    if zshift == 'first_layer':
-        z = c/2
-    # or move the center of mass to the middle of the cell
-    elif zshift == 'CM':
-        z = c/2 - d/2
-    shift_vector = np.array([0, 0, z])
-    shifted_basis = orig_basis + shift_vector
-    return shifted_basis.tolist()
-
-def get_basis_top(a, d, c, disregistry, zshift='CM'):
-
-    '''
-    `disregistry` is defined such that the distance to disregister from AB to AB again is 1.0,
-    which corresponds to 3*bond_length = 3/sqrt(3)*lattice_constant = sqrt(3)*lattice_constant
-    so we convert the given `disregistry` to angstrom
-    '''
-    disregistry_ang = 3**0.5*a*disregistry
-    orig_basis = np.array([
-        [0, a/3**0.5 + disregistry_ang, d],
-        [a/2, a/(2*3**0.5) + disregistry_ang, d]
-        ])
-
-    # for open boundary condition in the z-direction
-    # move the first layer to the middle of the cell
-    if zshift == 'first_layer':
-        z = c/2
-    # or move the center of mass to the middle of the cell
-    elif zshift == 'CM':
-        z = c/2 - d/2
-    shift_vector = np.array([0, 0, z])
-    shifted_basis = orig_basis + shift_vector
-    return shifted_basis.tolist()
-
-def create_graphene_geom_bottom(d,disregistry, a=2.462,c=15, zshift='CM'):
-    """
-    All units should be in angstroms"""
-    symbols = ['C', 'C']
-    return ase.Atoms(
-        symbols=symbols,
-        positions=get_basis_bottom(a, d, c, disregistry, zshift=zshift),
-        cell=get_lattice_vectors(a, c),
-        pbc=[1, 1, 1],
-        )
-
-def create_graphene_geom_top(d,disregistry, a=2.462,c=15, zshift='CM'):
-    """
-    All units should be in angstroms"""
-    symbols = ['C', 'C']
-    return ase.Atoms(
-        symbols=symbols,
-        positions=get_basis_top(a, d, c, disregistry, zshift=zshift),
-        cell=get_lattice_vectors(a, c),
-        pbc=[1, 1, 1],
-        )
-
-
 
 if __name__=="__main__":
     import ase.io
