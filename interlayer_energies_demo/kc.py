0874d57dbe4f9dde41176c7b70ad6dc490236760
Lucas Wagner <lucas.wagner@gmail.com>
2021-11-05 09:37:47 -0500

    Revert "add kc_only"
    This reverts commit c73e809d4884d234f3d3ed66bd7527bd56655f41.

--- interlayer_energies_demo/kc.py
+++ interlayer_energies_demo/kc.py
@@ -8,31 +8,6 @@ import os
 from ase.calculators.lammpsrun import LAMMPS
 import load
 
-def get_params_hybrid():
-    parameters = {
-        'pair_style': 'hybrid/overlay rebo kolmogorov/crespi/full 16.0 1',
-        'pair_coeff': [
-            '* * rebo CH.rebo C C',
-            '* * kolmogorov/crespi/full CH_taper.KC C C'
-            ],
-        'atom_style': 'full',
-        'specorder': ['C', 'C'],
-        }
-    files = ['CH.rebo', 'CH_taper.KC']
-    return parameters, files
-
-def get_params_kc_only():
-    parameters = {
-        'pair_style': 'hybrid/overlay kolmogorov/crespi/full 16.0 1',
-        'pair_coeff': [
-            '* * kolmogorov/crespi/full CH_taper.KC C C'
-            ],
-        'atom_style': 'full',
-        'specorder': ['C', 'C'],
-        }
-    files = ['CH_taper.KC']
-    return parameters, files
-
 def eval_energy(df, z0, C0, C2, C4, C, delta, lamda, A):
     """
     """
@@ -42,12 +17,21 @@ def eval_energy(df, z0, C0, C2, C4, C, delta, lamda, A):
         print(row['d'], row['disregistry'])
         atoms = generate_geometry.create_graphene_geom(row['d'], row['disregistry'])
         atoms.set_array('mol-id', np.array([0, 0, 1, 1]))
-        parameters, files = get_params_kc_only()
+        parameters = {
+            'pair_style': 'hybrid/overlay rebo kolmogorov/crespi/full 16.0 1',
+            'pair_coeff': [
+                # '* * none',
+                '* * rebo CH.rebo C C',
+                '* * kolmogorov/crespi/full CH_taper.KC C C'
+                ],
+            'atom_style': 'full',
+            'specorder': ['C', 'C'],
+            }
+        files = ['CH.rebo', 'CH_taper.KC']
         atoms.calc = LAMMPS(parameters=parameters, files=files, keep_tmp_files=True, tmp_dir='tmp/')
         e = atoms.get_potential_energy()/len(atoms)
         print(e)
         energy.append(e)
-        break
 
     lj_en =  np.asarray(energy) - np.min(energy) + np.min(df['energy'])
     return lj_en
